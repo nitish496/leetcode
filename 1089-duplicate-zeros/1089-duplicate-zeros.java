62f3593bb672c0dd0ea9ebe79dc2ed559829d2bf
Nitish Tammindi <mbeastreacts471@gmail.com>
2026-09-15 22:54:44 +0530

    Time: 1 ms (99.47%), Space: 46.7 MB (89.97%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int[] dest = new int[arr.length];
+
+        int d=0;
+        int s=0;
+        while(s<arr.length){
+            if(arr[s]==0){
+                if(d<arr.length){
+                  dest[d]=0;
+                }
+                d+=1;
+                if(d<arr.length){
+                  dest[d]=0;
+                }
+
+            }else{
+                if(d<arr.length){
+                  dest[d]=arr[s];
+                }
+
+            }
+         
+            d+=1;
+            s+=1;
+
+
+        }
+                    for(int i=0;i<arr.length;i++){
+                arr[i]=dest[i];
+            }
+
+    }
+}
